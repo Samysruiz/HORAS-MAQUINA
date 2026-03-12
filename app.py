7b16a16290d6164c9c5482b4fe349da7c6ab85c3
Samysruiz <samysruiz@gmail.com>
2026-03-12 09:01:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if menu == "Empregadores":
     st.subheader("Cadastrar empregador")
     empresa = st.text_input("Empresa")
     whats   = st.text_input("WhatsApp")
-    valor   = st.number_input("Valor hora (R$)", min_value=0.0, value=120.0, step=0.5)
+    valor   = st.number_input("Valor hora (R$)", min_value=30.0, value=120.0, step=0.5)
     if st.button("💾 Salvar"):
         novo = pd.DataFrame([{"empresa": empresa, "whats": whats, "valor_hora": valor}])
         emp  = pd.concat([emp, novo], ignore_index=True)
